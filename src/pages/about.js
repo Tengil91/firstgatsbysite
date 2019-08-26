4dbfb0ad405e323c9a5105c7792abb736de12715
Niklas Silfverhielm <kachiun91@gmail.com>
2019-08-26 21:41:50 +0200

    la till headerimg till contact och about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,10 @@ import Nav from './../components/nav';
 import { containerStyles } from '../styles/styles';
 export default () => (
   <div style={containerStyles}>
+    <FullImageHeader url={headerImg}>
+      <h1 style={{color: '#fff', fontSize: 48}}>Välkommen</h1>
+      <p>Detta är en sida där jag lägger upp lite av det jag håller på med.</p>
+    </FullImageHeader>
     <Header headerText="Om min sida" />
     <Nav />
     <p>Den är skitcool.</p>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,10 @@ import { containerStyles } from '../styles/styles';
 
 export default () => (
   <div style={containerStyles}>
+    <FullImageHeader url={headerImg}>
+      <h1 style={{color: '#fff', fontSize: 48}}>Välkommen</h1>
+      <p>Detta är en sida där jag lägger upp lite av det jag håller på med.</p>
+    </FullImageHeader>
     <Header headerText="Kontakta mig bara om det är viktigt" />
     <Nav />
     <p>Kontaktuppgifter kommer kanske någon gång. Snart!</p>
